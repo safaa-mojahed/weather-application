1201afb480869a3272dc899d1da1339e9dec8c02
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-01-26 17:14:01 +0200

    update login validation

--- src/app/pages/login-form/login-form.component.ts
+++ src/app/pages/login-form/login-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {WeatherService} from 'src/app/services/weather.service';
-import { FormBuilder } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -9,13 +10,21 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
-  checkoutForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  loginForm: FormGroup;
+  email:  FormControl;
+  password: FormControl;
+
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength[10], Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}')]),
+    });
+  }
+
+  revert() {
+    this.loginForm.reset();
   }
 
   loginUser(e) {
@@ -23,7 +32,6 @@ export class LoginFormComponent implements OnInit {
     let username = e.target.elements[0].value;
     let password = e.target.elements[1].value;
     this.weatherService.authentic(username, password);
-    this.checkoutForm.reset();
   }
 
 }
